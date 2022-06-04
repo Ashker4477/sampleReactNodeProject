84d195f600de41accb3e72a8004a1d160b66c522
Kevin Allen <kallen@aimabms.com>
2022-06-04 18:42:07 +0530

    post request is added

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,30 @@ app.use(cors({
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }))
 
+app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 app.use(express.json());
-
 
 app.get('/movies/data', (req, res)=>{
     return res.send(movies)
 });
-app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+
+app.post('/movies/data', (req, res)=>{
+  if (!req.body.name || !req.body.released){
+    return res.status(400).json({'error' : "Invalid Data"})
+  }
+  else{
+    let movie = {
+      id : movies.length + 1,
+      name : req.body.name,
+      released : req.body.released,
+    }
+    movies.push(movie);
+    return res.send(movie);
+  }
+})
 
 app.get('/*', (req,res)=>{
   res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"))
 })
-
 
 app.listen(PORT,()=>{`Server is listening on Port ${PORT}`})
